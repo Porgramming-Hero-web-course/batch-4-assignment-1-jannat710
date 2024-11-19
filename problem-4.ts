f578686daf5fc2b60f73951a65d3920ce16c4271
e <jannatul0040@gmail.com>
2024-11-19 21:08:25 +0600

    Solved problem-4

--- problem-4.ts
+++ problem-4.ts
@@ -1,3 +1,31 @@
 {
-    
+    type Circle = {
+        shape: "circle";
+        radius: number;
+    }
+    type Rectangle = {
+        shape: "rectangle";
+        width: number;
+        height: number;
+    }
+
+    type Shape = Circle | Rectangle;
+
+    const calculateShapeArea = (data: Shape) => {
+        if ('radius' in data) {
+            return parseFloat((Math.PI * data.radius * data.radius).toFixed(2));
+        } else if ('width' in data && 'height' in data) {
+            return (data.width * data.height);
+        }else{
+            throw new Error('Please give numbers')
+        }
+    }
+
+    const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+
+    const rectangleArea = calculateShapeArea({
+        shape: "rectangle",
+        width: 4,
+        height: 6,
+      });
 }
