762969899ee9ddc0a14ad0e9054835f1ea7def08
e <jannatul0040@gmail.com>
2024-11-20 11:01:24 +0600

    Solved Problem-7

--- problem-1.ts
+++ problem-1.ts
@@ -1,11 +1,6 @@
 {
     const sumArray = (arr: number[]): number => {
-        let sum: number = 0;
-        let i: number;
-        for (i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        return sum;
+        return arr.reduce((acc, element) => acc + element, 0);
     }
     const result = sumArray([1, 2, 3, 4, 5]);
 
--- problem-7.ts
+++ problem-7.ts
@@ -1,3 +1,21 @@
 {
-    
+    class Car {
+        make: string;
+        model: string;
+        year: number;
+
+        constructor(make: string, model: string, year: number) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+        getCarAge(): number {
+            const currentYear : number = new Date().getFullYear();
+            const carAge : number = currentYear - this.year;
+            return carAge;
+        }
+    }
+    const car = new Car("Honda", "Civic", 2018);
+
+
 }
